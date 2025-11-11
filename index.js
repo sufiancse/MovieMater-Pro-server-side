baa68120b68ff846d1411c6e4c7d0a151b33196d
sufuancse <sufian.cse24@gmail.com>
2025-11-12 02:11:58 +0600

    database setup complete, create movies,latest-movies, top rated movies api create complete

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vaa5xch.mongodb.net/?appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -21,16 +20,37 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const db = client.db("movie-master-pro-db");
+    const movieCollections = db.collection("movies");
+
+    // movie collections
+    app.get("/api/movies", async (req, res) => {
+      const cursor = movieCollections.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/api/latest-movies", async (req, res) => {
+      const cursor = movieCollections.find().sort({ releaseYear: -1 }).limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/api/top-rated-movies", async (req, res) => {
+      const cursor = movieCollections.find().sort({ rating: -1 }).limit(5);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+
+    // testing database, comment this section before deploy in vercel
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
